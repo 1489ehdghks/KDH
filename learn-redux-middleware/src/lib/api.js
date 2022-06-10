0e501bc0b1db7eb08c7e6551f752d62f53ffc339
kimdonghwan <1489ehdghks@gmail.com>
2022-06-10 18:48:23 +0900

    Commit before eject

--- learn-redux-middleware/src/lib/api.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const getPost = id =>
-    axios.get(`https://jsonplaceholder.typicode.com/post/${id}`);
-export const getUser = id =>
-    axios.get(`https://jsonplaceholder.typicode.com/users`);
